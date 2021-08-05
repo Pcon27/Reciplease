1eeafed5c0b24b4b37fc9ba11aecf04095f3a024
pconsag <pconsagra@g.ucla.edu>
2021-08-05 12:53:01 -0700

    update user routes, and add basis for profile routes

---
+++ controllers/profile-routes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Recipe, User, User_Recipe } = require('../models');
+const withAuth = require('../utils/auth');
+
+// GET all posts associated with the logged-in user
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const dbUserData = await Recipe.findAll(req.session.user_id, {
+      order: [['created_at', 'DESC']],
+
+      include: [{ model: User, through: User_Recipe}]
+    });
+        // serialize data before passing to template
+        const Recipe = dbUserData.map(post => post.get({ plain: true }));
+
+        res.render('profile', {
+          recipes,
+          username: req.session.username,
+          email: req.session.email,
+          user_image: req.session.user_image,
+          user_id: req.session.user_id,
+
+          loggedIn: true
+        });
+      }
+      catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+      };
+});
+
+
+// GET selected post for edit-post page
+router.get('/:id', async (req, res) => {
+  try {
+    const dbUserData = await Recipe.findAll(req.params.user_id, {
+      order: [['created_at', 'DESC']],
+
+      include: [{ model: User, through: User_Recipe}]
+    });
+        // serialize data before passing to template
+        const Recipe = dbUserData.map(post => post.get({ plain: true }));
+
+        res.render('profile', {
+          recipes,
+          username: req.session.username,
+          email: req.session.email,
+          user_image: req.session.user_image,
+          user_id: req.session.user_id,
+
+          loggedIn: true
+        });
+      }
+      catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+      };
+});
+    
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -4,11 +4,21 @@ const User_Recipe = require('./User_Recipe');
 
 // User/Recipe associations
 User.hasMany(Recipe, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    through: {
+        model: User_Recipe,
+        //field referenced in the association must have a unique constraint placed on it. 
+        unique: false
+      },
 });
 
 Recipe.belongsTo(User, {
     foreignKey: 'user_id',
+    through: {
+        model: User_Recipe,
+        //field referenced in the association must have a unique constraint placed on it. 
+        unique: false
+      },
 });
 
 // how to do "join tables"
