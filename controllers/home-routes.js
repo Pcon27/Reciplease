bbf256793826cdb38d38c553698530095b4a7081
EajayD <edelos1524@hotmail.com>
2021-08-07 12:58:39 -0700

    login handlebars to show

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,5 +31,17 @@ router.get('/', async (req, res) => {
     };
 });
 
+// Login route
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect to the homepage
+    console.log("req.session.loggedIn", req.session.loggedIn)
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    // Otherwise, render the 'login' template
+    res.render('login');
+  });
+
 
 module.exports = router;
