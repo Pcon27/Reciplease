ab7d0013f3bae62550117a3b7c8f72ad5d6313d2
JessGiannini <jessgiannini@gmail.com>
2021-08-08 15:16:23 -0700

    add recipe form

--- controllers/api/recipe-routes.js
+++ controllers/api/recipe-routes.js
@@ -1,27 +1,26 @@
-const router = require('express').Router();
-const { Recipe, User_Recipe, User } = require('../../models');//double check models
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Recipe, User_Recipe, User } = require("../../models"); //double check models
+const withAuth = require("../../utils/auth");
 
 //post a new recipe
-router.post('/', withAuth, async (req, res) => {
-    try {
-
-        const dbRecipeData = await Recipe.create({
-            user_id: req.session.userId,
-            name: req.body.name,
-            description: req.body.description,
-            ingredients: req.body.ingredients,
-            instructions: req.body.instructions,
-        });
-        const dbId = dbRecipeData.id;
-        await User_Recipe.create({
-            user_id: req.session.userId,
-            recipe_id: dbId
-        });
-        res.status(200).json(dbRecipeData);
-    } catch (err) {
-        res.status(500).json(err);
-    };
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const dbRecipeData = await Recipe.create({
+      user_id: req.session.userId,
+      name: req.body.name,
+      description: req.body.description,
+      ingredients: req.body.ingredients,
+      instructions: req.body.instructions,
+    });
+    const dbId = dbRecipeData.id;
+    await User_Recipe.create({
+      user_id: req.session.userId,
+      recipe_id: dbId,
+    });
+    res.status(200).json(dbRecipeData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -55,11 +55,16 @@ router.get("/users/:id", async (req, res) => {
         },
       ],
     });
+    const User_Recipe = dbUserData.get({ plain: true });
+    res.render("profile", {
+      User_Recipe,
+      loggedIn: true,
+    });
     if (!dbUserData) {
       res.status(404).json({ message: "No user found with this id" });
       return;
     }
-    res.json(dbUserData);
+    // res.json(dbUserData);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
